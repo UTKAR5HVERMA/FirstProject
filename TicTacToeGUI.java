44e6650c77a45db7f6bdc8a38267187116415f41
Utkarsh verma <123299202+UTKAR5HVERMA@users.noreply.github.com>
2023-09-01 00:00:24 +0530

    Add files via upload

---
+++ TicTacToeGUI.java
@@ -0,0 +1,113 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TicTacToeGUI extends JFrame {
+    private JButton[][] buttons;
+    private char currentPlayer;
+
+    public TicTacToeGUI() {
+        buttons = new JButton[3][3];
+        currentPlayer = 'X';
+
+        initializeUI();
+    }
+
+    private void initializeUI() {
+        setTitle("Tic Tac Toe");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(3, 3));
+
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                buttons[row][col] = new JButton();
+                buttons[row][col].setFont(new Font("Arial", Font.PLAIN, 48));
+                buttons[row][col].addActionListener(new ButtonClickListener(row, col));
+                add(buttons[row][col]);
+            }
+        }
+
+        setSize(300, 300);
+        setVisible(true);
+    }
+
+    private class ButtonClickListener implements ActionListener {
+        private int row;
+        private int col;
+
+        public ButtonClickListener(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (buttons[row][col].getText().equals("")) {
+                buttons[row][col].setText(String.valueOf(currentPlayer));
+                buttons[row][col].setEnabled(false);
+                if (checkWin(currentPlayer)) {
+                    JOptionPane.showMessageDialog(null, "Player " + currentPlayer + " wins!");
+                    resetGame();
+                } else if (isBoardFull()) {
+                    JOptionPane.showMessageDialog(null, "It's a draw!");
+                    resetGame();
+                } else {
+                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                }
+            }
+        }
+    }
+
+    private boolean checkWin(char player) {
+        // Check rows, columns, and diagonals for a win
+        for (int i = 0; i < 3; i++) {
+            if (buttons[i][0].getText().equals(String.valueOf(player)) &&
+                buttons[i][1].getText().equals(String.valueOf(player)) &&
+                buttons[i][2].getText().equals(String.valueOf(player))) {
+                return true; // Row win
+            }
+            if (buttons[0][i].getText().equals(String.valueOf(player)) &&
+                buttons[1][i].getText().equals(String.valueOf(player)) &&
+                buttons[2][i].getText().equals(String.valueOf(player))) {
+                return true; // Column win
+            }
+        }
+        if (buttons[0][0].getText().equals(String.valueOf(player)) &&
+            buttons[1][1].getText().equals(String.valueOf(player)) &&
+            buttons[2][2].getText().equals(String.valueOf(player))) {
+            return true; // Diagonal win
+        }
+        if (buttons[0][2].getText().equals(String.valueOf(player)) &&
+            buttons[1][1].getText().equals(String.valueOf(player)) &&
+            buttons[2][0].getText().equals(String.valueOf(player))) {
+            return true; // Diagonal win
+        }
+        return false;
+    }
+
+    private boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (buttons[i][j].getText().equals("")) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void resetGame() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+                buttons[i][j].setEnabled(true);
+            }
+        }
+        currentPlayer = 'X';
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new TicTacToeGUI());
+    }
+}
